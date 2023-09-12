059e04bbb60d27e0e4cadd890826d8d1ce78d2c0
azzzub <yuanaayubs@gmail.com>
2023-09-12 14:53:15 +0700

    feat: add docker

--- utils/localAdmin.js
+++ utils/localAdmin.js
@@ -9,22 +9,26 @@ const prisma = new PrismaClient();
 async function localAdmin() {
   const hash = await bcrypt.hash("admin", 10);
 
-  const team = await prisma.team.create({
-    data: {
-      name: "Team Admin",
-    },
-  });
+  try {
+    const team = await prisma.team.create({
+      data: {
+        name: "Team Admin",
+      },
+    });
 
-  const res = await prisma.user.create({
-    data: {
-      username: "admin",
-      password: hash,
-      role: 2,
-      teamID: team.id,
-    },
-  });
+    const res = await prisma.user.create({
+      data: {
+        username: "admin",
+        password: hash,
+        role: 2,
+        teamID: team.id,
+      },
+    });
 
-  console.log(JSON.stringify(res, null, 2));
+    console.log(JSON.stringify(res, null, 2));
+  } catch (error) {
+    console.error(error);
+  }
 
   prisma.$disconnect();
 }
