983d5cc45a293c924334b069b6454b3ee3c59dd2
azzzub <yuanaayubs@gmail.com>
2024-01-24 16:22:02 +0700

    feat: flush

---
+++ utils/flush.js
@@ -0,0 +1,47 @@
+// APPROVAL STATUS:
+// 0  - PENDING
+// 1  - APPROVED
+// 10 - APPROVE_EXPIRED
+// 2  - PENDING_EXPIRED
+// 3  - REJECTED
+
+// External
+const { PrismaClient } = require("@prisma/client");
+const fs = require("fs");
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const snapshot = await prisma.snapshot.findMany({
+    where: {
+      OR: [
+        {
+          approval: 10,
+        },
+        {
+          approval: 2,
+        },
+        {
+          approval: 3,
+        },
+      ],
+    },
+  });
+  snapshot.forEach((v) => {
+    const filename = __dirname + "/../public/snapshots/" + v.filename;
+    fs.unlink(filename, (err) => {
+      if (err) {
+        // Handle specific error if any
+        if (err.code === "ENOENT") {
+          console.error("File does not exist.");
+        } else {
+          throw err;
+        }
+      } else {
+        console.log("File deleted!" + v.filename);
+      }
+    });
+  });
+}
+
+main();
